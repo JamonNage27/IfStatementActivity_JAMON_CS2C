0dfe61bfe78bc6f4798f88beafa83b5d45beb8d5
JamonNage27 <nagelygansaenjamon@gmail.com>
2024-10-03 22:44:42 +0800

    ageCheck.js

---
+++ ageCheck.js
@@ -0,0 +1,9 @@
+let age = 14;
+let EnteryourAge = age
+
+console.log("Enter Your Age:", EnteryourAge);
+if (age < 13){console.log("You are a child.");}
+else if(age <20){console.log("You are a teenager.")}
+else
+{console.log("You are an  adult")
+}
